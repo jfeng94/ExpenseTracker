20e12e9fd5e1e9e9db372bd5fa132109f7a8c869
Jerry Feng <jfeng94@gmail.com>
2018-09-15 23:31:43 -0700

    Initial Commit

---
+++ Expense Tracker/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Expense Tracker
+//
+//  Created by Jerry Feng on 9/15/18.
+//  Copyright © 2018 Jerry Feng. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
